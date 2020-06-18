aba2f3cc14359c7dd32f7b075236dc7654f967ac
danila <danlun14@mail.ru>
2020-06-18 13:55:16 +0700

    create a function, which write words from file into the programm

--- src/command.c
+++ src/command.c
@@ -11,12 +11,19 @@ int sort_file(const char* input_file, const char* output_file, int sort_type)
     if (out != NULL) {
         printf("Wrong output file name");
         fclose(out);
+        fclose(in);
+        return -1;
     }
-    FILE* out = fopen(output_file, "w");
+    out = fopen(output_file, "w");
     if (out == NULL) {
         fclose(in);
         return -1;
     }
 
-    dictionary* dic = dic_init;
+    dictionary* dic;
+    dic = dic_init();
+
+    fill_dic(dic, in);
+    printf("%s", dic->str[2].s);
+    return 0;
 }
