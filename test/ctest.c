c2d0da176f74dde06040d88c389298336d42c98a
danila <danlun14@mail.ru>
2020-06-23 20:02:27 +0700

    add test for str_to_dic

--- test/ctest.c
+++ test/ctest.c
@@ -11,3 +11,27 @@ CTEST(arithmetic_suite, dic_init)
     ASSERT_EQUAL(i, 0);
     dic_free(&dic1);
 }
+
+CTEST(arithmetic_suite, str_to_dic)
+{
+    dictionary dic1;
+    dic_init(&dic1, 65655);
+    char str[4] = {'1', 'c', 'd'};
+    int prev = dic1.size_s;
+    prev++;
+    str_to_dic(&dic1, str);
+    int cur = dic1.size_s;
+    ASSERT_EQUAL(prev, cur);
+    prev++;
+    str_to_dic(&dic1, str);
+    cur = dic1.size_s;
+    ASSERT_EQUAL(prev, cur);
+    prev++;
+    str_to_dic(&dic1, str);
+    cur = dic1.size_s;
+    ASSERT_EQUAL(prev, cur);
+    prev++;
+    str_to_dic(&dic1, str);
+    cur = dic1.size_s;
+    ASSERT_EQUAL(prev, cur);
+}
